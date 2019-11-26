08073eafafe724e2a7f71086c512979e9f6c6a7c
revortni <rojit.blaze13@gmail.com>
2019-11-27 03:52:34 +0545

    added automatic delay for sliding

--- JS-PROJECTS/ast1/script.js
+++ JS-PROJECTS/ast1/script.js
@@ -1,88 +1,91 @@
+var imageChangeInterval;
+var slideInterval;
+var waitAfterSlideButtonPress;
+var leftButton = document.getElementById('control-l');
+var rightButton = document.getElementById('control-r');
+var IMAGE_WIDTH = 300;
 var imageContainer = document.getElementsByClassName(
   'carousel-image-wrapper'
 )[0];
 var images = Array.from(imageContainer.getElementsByTagName('img'));
 var totalImageCount = images.length;
 var currentImageIndex = 0;
-var leftButton = document.getElementById('control-l');
-var rightButton = document.getElementById('control-r');
-var imageChangeInterval;
-var slideInterval;
-var waitAfterSlideButtonPress;
-var IMAGE_WIDTH = 300;
 
-function addBulletNavigation() {
-  var parent = document.getElementById('bulletNavigation');
-  images.forEach(function(value, index) {
+function addIndicatorNav() {
+  var parent = document.getElementById('indicator-nav');
+  images.forEach(function(val, index) {
     var bullet = document.createElement('span');
     bullet.position = index;
     bullet.addEventListener('click', function() {
       pauseAutoSlideAfterButtonPress();
-      setSlideImage(this.position);
+      animateSlide(this.position);
     });
     parent.appendChild(bullet);
   });
 }
 
-function getAbsoluteIndex(position) {
-  return (position + totalImageCount) % totalImageCount;
-}
-
-/* Set index of image to be shown */
-function setSlideImage(position = 0) {
-  images.forEach(function(image, index) {
-    image.style.left = (index - position) * IMAGE_WIDTH + 'px';
+function setIndicatorDot(position) {
+  var parent = document.getElementById('indicator-nav');
+  var indicator = Array.from(parent.children);
+  indicator.forEach(function(val, index) {
+    if (position == index) {
+      val.classList.add('active');
+      return;
+    }
+    val.classList.remove('active');
   });
 }
-function animateSlide() {
-  var shift = 0;
-  var factor = 1;
-  if (currentImageIndex >= totalImageCount - 1) {
-    factor = -currentImageIndex;
+
+/* change image with animated sliding */
+function animateSlide(destinationIndex) {
+  var slideFactor = 0;
+  if (destinationIndex < 0) {
+    destinationIndex = totalImageCount - 1;
   }
+  destinationIndex = destinationIndex % totalImageCount;
+  setIndicatorDot(destinationIndex);
+  var currentPosition = -currentImageIndex * IMAGE_WIDTH;
+  var displacement = (destinationIndex - currentImageIndex) * IMAGE_WIDTH;
   slideInterval = setInterval(function() {
-    images.forEach(function(image, index) {
-      image.style.left =
-        (index - currentImageIndex - factor * shift) * IMAGE_WIDTH + 'px';
-    });
-    shift += 0.2;
-    if (shift > 1) {
+    imageContainer.style.left =
+      currentPosition - displacement * slideFactor + 'px';
+    slideFactor += 0.2;
+    if (slideFactor > 1) {
+      currentImageIndex = destinationIndex;
       clearInterval(slideInterval);
-      currentImageIndex = getAbsoluteIndex(currentImageIndex + 1);
     }
-  }, 50);
+  }, 100);
 }
 
 /* Slide image after 2s */
 function automaticSlideAfterDelay() {
   imageChangeInterval = setInterval(function() {
-    animateSlide();
+    animateSlide(currentImageIndex + 1);
   }, 2000);
 }
 
+/* Stop all async tasks and resume automatic slide*/
 function pauseAutoSlideAfterButtonPress() {
   clearInterval(slideInterval);
-  clearTimeout(waitAfterSlideButtonPress);
   clearInterval(imageChangeInterval);
+  clearTimeout(waitAfterSlideButtonPress);
   waitAfterSlideButtonPress = setTimeout(function() {
     automaticSlideAfterDelay();
-  }, 2000);
+  }, 0);
 }
 
 /* Slide Button Controls */
 leftButton.onclick = function() {
-  currentImageIndex = getAbsoluteIndex(currentImageIndex - 1);
-  animateSlide(currentImageIndex);
   pauseAutoSlideAfterButtonPress();
+  animateSlide(currentImageIndex - 1);
 };
 
 rightButton.onclick = function() {
-  currentImageIndex = getAbsoluteIndex(currentImageIndex + 1);
-  animateSlide(currentImageIndex);
   pauseAutoSlideAfterButtonPress();
+  animateSlide(currentImageIndex + 1);
 };
 
 /* Execution Start */
-addBulletNavigation();
-setSlideImage(0);
+imageContainer.style.width = IMAGE_WIDTH * totalImageCount + 'px';
+addIndicatorNav();
 automaticSlideAfterDelay();
