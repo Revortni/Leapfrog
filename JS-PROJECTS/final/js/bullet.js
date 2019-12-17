4cf0ae37e0a3cd511de68a4507bf75c28b47ff63
revortni <rojit.blaze13@gmail.com>
2019-12-17 07:34:58 +0545

    adjusted class methods

--- JS-PROJECTS/final/js/bullet.js
+++ JS-PROJECTS/final/js/bullet.js
@@ -6,17 +6,10 @@ class Bullet {
     this.dx = dx;
     this.dy = dy;
     this.speed = 5;
-    this.r = 2;
+    this.width = 2;
+    this.height = 2;
     this.destroyed = false;
     this.image = gameAssets.bullet.img;
-    this.init();
-  }
-
-  init() {
-    if (this.dy == 1 && this.jumping) {
-      //if jumping and shooting down
-      this.dx = 0;
-    }
   }
 
   move() {
@@ -29,10 +22,10 @@ class Bullet {
     c.beginPath();
     c.drawImage(
       this.image,
-      (this.x + this.r) * SCALE,
-      (this.y + this.r) * SCALE,
-      2 * this.r * SCALE,
-      2 * this.r * SCALE
+      (this.x + this.width) * SCALE,
+      (this.y + this.height) * SCALE,
+      2 * this.width * SCALE,
+      2 * this.height * SCALE
     );
     c.closePath();
   }
@@ -47,5 +40,26 @@ class Bullet {
     ) {
       this.destroyed = true;
     }
+  };
+
+  checkCollision = targets => {
+    targets.forEach(target => {
+      if (
+        target.x < this.x + this.width &&
+        this.x < target.x + target.width &&
+        target.y < this.y + this.height &&
+        this.y < target.y + target.height
+      ) {
+        this.destroyed = true;
+        if (target instanceof Player) {
+          target.alive = false;
+        } else {
+          target.hp--;
+          if (target.hp <= 0) {
+            target.alive = false;
+          }
+        }
+      }
+    });
   };
 }
