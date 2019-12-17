cc23c3f0736b3f372ae3d102b7113f862bd3a509
revortni <rojit.blaze13@gmail.com>
2019-12-17 07:38:37 +0545

    stationary sniper , need adjustment for bullet

--- JS-PROJECTS/final/js/sniper.js
+++ JS-PROJECTS/final/js/sniper.js
@@ -24,7 +24,7 @@ class Sniper extends Enemy {
     let angle = Math.atan2(y - this.y, x - this.x);
     let dx = Math.cos(angle);
     let dy = Math.sin(angle);
-    let bullet = new EnemyBullet(this.world, this.x, this.y, dx, dy);
+    let bullet = new Bullet(this.x, this.y, dx, dy);
     this.world.enemyBullets.push(bullet);
   }
 
@@ -42,49 +42,5 @@ class Sniper extends Enemy {
     ctx.closePath();
   };
 
-  checkBoundary = () => {};
-}
-class EnemyBullet {
-  constructor(world, x, y, dx, dy) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.speed = 2;
-    this.r = 2;
-    this.destroyed = false;
-    this.image = gameAssets.bullet.img;
-    this.world = world;
-  }
-
-  move() {
-    this.x += this.dx * this.speed;
-    this.y += this.dy * this.speed;
-    this.checkBoundary();
-  }
-
-  draw() {
-    let c = ctx;
-    c.beginPath();
-    c.drawImage(
-      this.image,
-      (this.x + this.r) * SCALE,
-      (this.y + this.r) * SCALE,
-      2 * this.r * SCALE,
-      2 * this.r * SCALE
-    );
-    c.closePath();
-  }
-
-  checkBoundary = () => {
-    //move bullets and bulletBoundaryCheck function
-    if (
-      this.x > SCREEN.width ||
-      this.x < 0 ||
-      this.y < 0 ||
-      this.y > SCREEN.height
-    ) {
-      this.destroyed = true;
-    }
-  };
+  checkScreenBoundary = () => {};
 }
